45ea4739925f059ba94556a2ea241b3c00f671df
Thomas Friedel <ralino96@googlemail.com>
2016-12-13 09:31:10 +0100

    Make wallpaper picker faster

--- .config/i3/helper/pick-wallpaper.sh
+++ .config/i3/helper/pick-wallpaper.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #
-# Might get very slow with large first argument. Use with care.
 
 num=0
 files="$HOME/wallpaper/*.png"
@@ -16,24 +15,15 @@ for img in $files; do
     let "num++"
 done
 
-if (( $num + 1 < $requested )); then
+if (( $num < $requested )); then
     echo "Not enough pictures to satisfy request."
     exit 1
 fi
 
 for req in `seq $requested`; do
-    result[$req]="NULL"
-    while [ ${result[$req]} == "NULL" ]; do
-        rand=$RANDOM
-        let "rand %= $num"
-        result[$req]=${fileArray[$rand]}
-        for prev in `seq $req`; do
-            if (( $prev != $req )); then
-                if [ ${result[$prev]} == ${result[$req]} ]; then
-                    result[$req]="NULL"
-                fi
-            fi
-        done
-    done
-    echo -n "${result[$req]} "
+    rand=$RANDOM
+    let "rand %= $num"
+    echo -n "${fileArray[$rand]} "
+    let "num--"
+    fileArray[$rand]=${fileArray[$num]}
 done
