d7977b853e79c13066a3adc9a6ac7e9d842f9095
Thomas Friedel <ralino96@googlemail.com>
2017-11-09 00:34:42 +0100

    Revert changes to toggle-clone

--- i3-config/helper/toggle-clone.sh
+++ i3-config/helper/toggle-clone.sh
@@ -3,27 +3,27 @@
 STATUS=$(xrandr -q)
 HORIZONTAL=$( echo "$STATUS" | grep -o -E "current [0-9]{4} x [0-9]{3}" | cut -b 9-13 )
 
-if [ -z "$( echo "$STATUS" | grep -o -E "HDMI-1 disconnected")" ]
+if [ -z "$( echo "$STATUS" | grep -o -E "HDMI1 disconnected")" ]
 then
-    echo "HDMI-1"
+    echo "HDMI1"
     if [  "$HORIZONTAL" -lt "1921" ]
     then
-        xrandr --output HDMI-1 --auto --primary --right-of eDP-1
+        xrandr --output HDMI1 --auto --primary --right-of eDP1
     else
-        xrandr --output HDMI-1 --auto --output eDP-1 --auto --same-as HDMI-1
+        xrandr --output HDMI1 --auto --output eDP1 --auto --same-as HDMI1
     fi
 else
-    if [ -z "$( echo "$STATUS" | grep -o -E "VGA-1 disconnected")" ]
+    if [ -z "$( echo "$STATUS" | grep -o -E "VGA1 disconnected")" ]
     then
-        echo "VGA-1"
+        echo "VGA1"
         if [  "$HORIZONTAL" -lt "1921" ]
         then
-            xrandr --output VGA-1 --auto --primary --right-of eDP-1
+            xrandr --output VGA1 --auto --primary --right-of eDP1
         else
-            xrandr --output VGA-1 --auto --output eDP-1 --auto --same-as VGA-1
+            xrandr --output VGA1 --auto --output eDP1 --auto --same-as VGA1
         fi
     else
-        xrandr --output eDP-1 --auto --primary --output HDMI-1 --off --output VGA-1 --off
+        xrandr --output eDP1 --auto --primary --output HDMI1 --off --output VGA1 --off
     fi
 fi
 
