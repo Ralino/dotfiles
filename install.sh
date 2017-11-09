7f1bfc5a4ef8696799ee7e74f3ecc3f0cd042cab
Thomas Friedel <ralino96@googlemail.com>
2017-11-09 01:52:34 +0100

    Allow install into already existing folders

--- install.sh
+++ install.sh
@@ -18,23 +18,43 @@ packages="
   ycm-conf.py          $HOME/.vim/.ycm_extra_conf.py
 "
 
-set -- $packages
-while [ $# -ge 1 ]; do
-  package="$(dirname "$0")/$1"
-  target="$2"
-  shift 2
-
-  if [ -h $target ] && [ "$(readlink -f $target)" -ef "$package" ]; then
-    echo "Target $target is already installed"
-    continue
+choice=n
+install-package() {
+  local file
+  local target
+  file=$1
+  target=$2
+  if [ -h $target ] && [ "$(readlink -f $target)" -ef "$(dirname "$0")/$file" ]; then
+    echo "$file is already installed"
+    return
   fi
 
   if [ -e $target ]; then
-    #TODO recursive descent if package and target are directories
-    echo "$(tput setaf 1)There already exists a different file at $target$(tput sgr0)"
-    continue
+    if [ -d $target ] && [ -d $file ]; then
+      for child in $(ls $file); do
+        install-package "$file/$child" "$target/$child"
+      done
+    else
+      echo "$(tput setaf 1)There already exists a different file at $target$(tput sgr0)"
+      return
+    fi
+  else
+    if ! ln -sr $(dirname "$0")/$1 $target; then
+      read -e -p "Failed to link to $target! May I try with root? [Y/n]" choice
+      if [[ "$choice" == [Nn]* ]]; then
+        return
+      fi
+      if ! sudo ln -sr $(dirname "$0")/$1 $target; then
+        echo "$(tput setaf 1)Failed to link $file!$(tput sgr0)"
+        return
+      fi
+    fi
+    echo "$(tput setaf 2)Installed $1$(tput sgr0)"
   fi
+}
 
-  ln -sr $package $target
-  echo "$(tput setaf 2)Installed package $(basename $package)$(tput sgr0)"
+set -- $packages
+while [ $# -ge 1 ]; do
+  install-package "$1" "$2"
+  shift 2
 done
