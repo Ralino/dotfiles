4129152b027a330ad6cf4dc67636ab03adc7e53d
Thomas Friedel <ralino96@googlemail.com>
2018-01-14 22:57:02 +0100

    Make workspace_manager easier to configure

--- i3-config/helper/workspace_manager.py
+++ i3-config/helper/workspace_manager.py
@@ -3,65 +3,68 @@
 import i3ipc
 import re
 
-i3 = i3ipc.Connection()
+
+termite_map = [
+    (".*\\(.*iboss_ws/src/iboss/.*\\) - VIM$", "iBOSS Vim"),
+    (".* - VIM$", "Vim"),
+    ("^.+?@nextcloud.*", "Nexcloud ssh"),
+    (".* - ROS2$", "ROS2 Terminal"),
+    (".* - Kamaro$", "Kamaro Terminal"),
+    ("^root@foxlair.*", "Root Terminal"),
+    ("^ranger:.*", "Ranger"),
+    # default
+    "Terminal"
+]
+
+# Precompile regex
+temp_list = []
+for i in range(len(termite_map) - 1):
+    temp_list.append((re.compile(termite_map[i][0]), termite_map[i][1]))
+temp_list.append((re.compile('.*'), termite_map[len(termite_map) - 1]))
+termite_map = temp_list
+
+class_map = {
+    "Nightly": "Browser",
+    "Firefox": "Browser",
+    "jetbrains-pycharm-ce": "PyCharm",
+    "whats-app-nativefier-7bbd2c": "WhatsApp",
+    "Termite": termite_map,
+    "Polybar": "",
+    "Cellwriter": "",
+    "Pavucontrol": ""
+}
+
 
 def get_ws_name(con):
     if not con.window:
         return "Empty"
-    else:
-        if con.window_class == "URxvt":
-            return get_urxvt_name(con)
-        elif con.window_class == "Chromium":
-            return get_chromium_name(con)
-        else:
-            if (con.window_class == "Pavucontrol" and con.type == "floating_con"
-                    or con.window_class == "Gsimplecal"):
-                return ""
-            return con.window_class
-
-def get_urxvt_name(con):
-    #TODO ranger
     if con.name == "scratchpad":
         return ""
-    if con.name.endswith(" - VIM"):
-        if con.name.startswith("stdin"):
-            return "Vimpager"
-        else:
-            return "Vim"
-    elif con.name.startswith("kamaro@nextcloud") or con.name.startswith("root@nextcloud"):
-        return "SSH Nextcloud"
-    elif con.name.startswith("kamaro@kamaro-beteigeuze"):
-        return "SSH Beteigeuze"
-    elif con.name.startswith("root@foxlair"):
-        return "Root Terminal"
-    else:
-        flag = re.search("(?<= - )\w+$", con.name)
-        if flag:
-            if (flag.group(0) == "ROS" or flag.group(0) == "BTR"
-                                      or flag.group(0) == "iBOSS"):
-                return flag.group(0) + " Terminal"
-            else:
-                return flag.group(0)
-        else:
-            return "Terminal"
+    if con.window_class not in class_map:
+        return con.window_class
 
-def get_chromium_name(con):
-    if con.name.endswith("WhatsApp"):
-        return "WhatsApp"
-    elif con.name.startswith("Signal"):
-        return "Signal"
-    elif con.name.startswith("Google Kalender"):
-        return "Calendar"
+    mapped_name = class_map[con.window_class]
+    if type(mapped_name) != str:
+        for pair in mapped_name:
+            if pair[0].fullmatch(con.name):
+                return pair[1]
+        #Should not get here, make aware if it does
+        return "DEFAULTMISSING"
     else:
-        return "Chromium"
+        return mapped_name
+
 
 def on_focus_change(i3, e):
    focused = i3.get_tree().find_focused()
    set_workspace_name(focused.workspace(), get_ws_name(focused))
 
+
 def set_workspace_name(ws, new_name):
     if new_name != "" and ws.name != "{0}: {1} ".format(ws.num, new_name):
         i3.command('rename workspace "{0}" to "{1}: {2} "'.format(ws.name, ws.num, new_name))
+
+
+i3 = i3ipc.Connection()
 
 i3.on('workspace::focus', on_focus_change)
 i3.on('window::focus', on_focus_change)
