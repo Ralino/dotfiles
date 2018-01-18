e29c138b11d996ef14e9c80ec87611cc3b8dfe97
Thomas Friedel <ralino96@googlemail.com>
2018-01-18 18:06:58 +0100

    Revert order-workspaces, i3 does not like it

--- i3-config/helper/polybar.sh
+++ i3-config/helper/polybar.sh
@@ -5,6 +5,7 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 0.1; done
 
+sleep 0.2
 if $basedir/dual-head.sh; then
   if [ "$(xrandr -q | grep -o -E "current [0-9]{4} x [0-9]{3}" | cut -b 9-13)" -gt "1920" ]; then
     polybar primary &
