5d007e156a550234e24f430807695cd32d0d2a14
Thomas Friedel <ralino96@googlemail.com>
2018-01-22 13:26:08 +0100

    Add pacman hooks to repo

--- install.sh
+++ install.sh
@@ -19,6 +19,7 @@ packages="
   Xresources           $HOME/.Xresources
   ycm-conf.py          $HOME/.vim/.ycm_extra_conf.py
   compton.conf         $HOME/.config/compton.conf
+  pacman-hooks         /etc/pacman.d/hooks
 "
 
 install-package() {
